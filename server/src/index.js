04f71ae2e6294e74e19458d388ebd839c8d3f40d
Vidyadheesh <itcell@git.edu>
2026-01-13 16:31:56 +0530

    CORS issue resolution

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,12 @@ const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://hyloc.git.edu', 'http://localhost:3000', 'http://localhost:5000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
